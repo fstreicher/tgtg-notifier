0791d4fe629bdd374e7205a17570a65dbb9f155d
Fabian Streicher <fabi.streicher@gmail.com>
2020-03-23 12:19:13 +0100

    feat(): use favorites instead of indivdual items

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const { getItem } = require('./lib/api');
-const express = require('express');
 require('./lib/cron');
 
-const app = express();
-
-app.get('/items', async (req, res, next) => {
-  console.info('Getting items...');
-  const item = await getItem();
-  const itemsAvailable = item.items_available;
-  if (itemsAvailable > 0) {
-    console.info(`${item.store.store_name} has ${item.items_available} items available.`);
-  } else {
-    console.info(`${item.store.store_name} has no items available.`);
-  }
-
-  res.json({ itemsAvailable });
-});
-
-app.listen(3030, () => {
-  console.info('Running on port 3030');
+process.on('SIGINT', () => {
+  console.info('\nShutting down gracefully...');
+  process.exit(1);
 });
