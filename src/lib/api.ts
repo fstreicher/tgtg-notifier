87230abfdcd543eb6e56eb61b28f18220af690df
Fabian Streicher <fabi.streicher@gmail.com>
2021-04-24 13:46:11 +0200

    refactor: use generic makeRequest method

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosPromise, AxiosRequestConfig } from 'axios';
-import { Item, LoginResponse, RefreshResponse } from '../types';
 
-const USER_AGENT = 'TooGoodToGo/21.3.0 (935) (iPhone/iPhone X (GSM); iOS 14.4.2; Scale/3.00)';
+import { ItemResponse, LoginResponse, RefreshResponse } from '../types';
+
 const BASE_URL = 'https://apptoogoodtogo.com';
 const PATH = {
   LOGIN: '/api/auth/v2/loginByEmail/',
@@ -10,67 +10,54 @@ const PATH = {
 };
 const HEADERS = {
   'Content-Type': 'application/json',
-  'User-Agent': USER_AGENT
+  'User-Agent': 'TooGoodToGo/21.3.0 (935) (iPhone/iPhone X (GSM); iOS 14.4.2; Scale/3.00)'
 };
 
 export abstract class ApiWrapper {
-  public static login(email: string, password: string): AxiosPromise<LoginResponse> {
+
+  private static makeRequest<T>(path: string, data: Record<string, any>, headers?: Record<string, string>): AxiosPromise<T> {
     const options: AxiosRequestConfig = {
       baseURL: BASE_URL,
-      url: PATH.LOGIN,
-      method: 'POST',
-      headers: HEADERS,
-      data: {
-        device_type: 'IOS',
-        email: email,
-        password: password
-      }
-    };
-
-    return axios(options);
-  }
-
-  public static refreshToken(token: string): AxiosPromise<RefreshResponse> {
-    const options: AxiosRequestConfig = {
-      baseURL: BASE_URL,
-      url: PATH.REFRESH,
-      method: 'POST',
-      headers: HEADERS,
-      data: {
-        refresh_token: token
-      }
-    };
-
-    return axios(options);
-  }
-
-
-  public static getFavorites(
-    userId: string,
-    authToken: string,
-    lat: string,
-    long: string
-  ): AxiosPromise<{ items: Array<Item> }> {
-    const options: AxiosRequestConfig = {
-      baseURL: BASE_URL,
-      url: PATH.ITEM,
+      url: path,
       method: 'POST',
       headers: {
         ...HEADERS,
-        'Authorization': `Bearer ${authToken}`
+        ...headers
       },
-      data: {
-        user_id: userId,
-        origin: {
-          latitude: lat,
-          longitude: long
-        },
-        radius: 0,
-        favorites_only: true
-      }
+      data: data
     };
 
     return axios(options);
+  }
 
+  public static login(email: string, password: string): AxiosPromise<LoginResponse> {
+    const data = {
+      device_type: 'IOS',
+      email: email,
+      password: password
+    }
+    return ApiWrapper.makeRequest(PATH.LOGIN, data);
+  }
+
+  public static refreshToken(token: string): AxiosPromise<RefreshResponse> {
+    return ApiWrapper.makeRequest(PATH.REFRESH, { refresh_token: token });
+  }
+
+
+  public static getFavorites(userId: string, authToken: string, lat: string, long: string): AxiosPromise<ItemResponse> {
+    const headers = {
+      'Authorization': `Bearer ${authToken}`
+    };
+    const data = {
+      user_id: userId,
+      origin: {
+        latitude: lat,
+        longitude: long
+      },
+      radius: 0,
+      favorites_only: true
+    }
+
+    return ApiWrapper.makeRequest(PATH.ITEM, data, headers);
   }
 }
