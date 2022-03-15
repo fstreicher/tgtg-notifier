d296f5c4153c7787c68339d1e7ac3d8d3bcd5728
Fabian Streicher <fabi.streicher@gmail.com>
2022-03-15 19:08:31 +0100

    fix: remove debug code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { scheduleCronJobs } from './lib/cron';
-import { getLoginPin } from './lib/gmail';
 
 scheduleCronJobs();
-// getLoginPin().then(res => console.log('index', res));
 
 process.on('SIGINT', () => {
   console.info('\nShutting down gracefully...');
--- src/lib/cron.ts
+++ src/lib/cron.ts
@@ -3,21 +3,17 @@ import { schedule } from 'node-cron';
 import { scrapeFavorites } from './scraper';
 
 export function scheduleCronJobs(): void {
-  // // run every 5 minutes, except from 1800-1900
-  // schedule('*/5 0-17,19-23 * * *', () => {
-  //   const date = new Date();
-  //   console.log(`\nRunning cron job @ ${date.toLocaleTimeString('de-DE', { hour12: false })}`);
-  //   scrapeFavorites();
-  // });
+  console.info('Initializing cron jobs')
 
-  // // run every minute from 1800-1859
-  // schedule('* 18 * * *', () => {
-  //   const date = new Date();
-  //   console.log(`\nRunning cron job @ ${date.toLocaleTimeString('de-DE', { hour12: false })}`);
-  //   scrapeFavorites();
-  // });
- 
-  schedule('* * * * *', () => {
+  // run every 5 minutes, except from 1800-1900
+  schedule('*/5 0-17,19-23 * * *', () => {
+    const date = new Date();
+    console.log(`\nRunning cron job @ ${date.toLocaleTimeString('de-DE', { hour12: false })}`);
+    scrapeFavorites();
+  });
+
+  // run every minute from 1800-1859
+  schedule('* 18 * * *', () => {
     const date = new Date();
     console.log(`\nRunning cron job @ ${date.toLocaleTimeString('de-DE', { hour12: false })}`);
     scrapeFavorites();
