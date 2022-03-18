3dc4a05dd389c0209ce8fc86bec0554250472aa1
Fabian Streicher <fabi.streicher@gmail.com>
2022-03-18 18:43:11 +0100

    style: reformat url

--- src/lib/pushover.ts
+++ src/lib/pushover.ts
@@ -4,9 +4,17 @@ import { PushoverPriority } from '../types';
 
 export function pushover(appToken: string, userKey: string, title: string, message: string, prio = PushoverPriority.NORMAL): void {
 
+  let url = '/1/messages.json';
+  url += `?token=${encodeURIComponent(appToken)}`;
+  url += `&user=${encodeURIComponent(userKey)}`;
+  url += `&title=${encodeURIComponent(title)}`;
+  url += `&message=${encodeURIComponent(message)}`;
+  url += `&html=1`;
+  url += `&priority=${prio}`;
+
   const options: AxiosRequestConfig = {
     baseURL: 'https://api.pushover.net',
-    url: `/1/messages.json?token=${encodeURIComponent(appToken)}&user=${encodeURIComponent(userKey)}&title=${encodeURIComponent(title)}&message=${encodeURIComponent(message)}&priority=${prio}`,
+    url,
     method: 'POST'
   };
 
