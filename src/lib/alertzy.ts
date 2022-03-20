53e4314370df6a6db3518ec9a7d6bd2d7b838848
Fabian Streicher <fabi.streicher@gmail.com>
2022-03-20 22:37:13 +0100

    fix: catch errors

--- src/lib/alertzy.ts
+++ src/lib/alertzy.ts
@@ -11,5 +11,10 @@ export function alertzy(key: string, title: string, message: string, prio: Alert
   };
 
   axios.request(options)
+    .then(res => {
+      if (res.data?.response === 'fail') {
+        console.error(res.data.error);
+      }
+    })
     .catch(err => console.error(err));
 }
